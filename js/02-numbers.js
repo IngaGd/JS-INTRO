8f8598c1b4c101341478ad603cbc5e3a9c14018b
Inga <inga.gudaite@gmail.com>
2022-12-09 16:51:48 +0200

    Numbers

---
+++ js/02-numbers.js
@@ -0,0 +1,162 @@
+console.log(123);
+
+const PI = 3.14; //matematines reiksmes pagal nutylejima rasomos didziosiomis
+console.log(PI);
+
+let walet = 0;
+console.log(walet);
+
+walet = 5;
+console.log(walet);
+
+walet = 10;
+console.log(walet);
+
+const negative = -4566;
+console.log(negative);
+
+const maxNumber = Infinity;
+const minNumber= -Infinity;
+
+console.log(maxNumber, minNumber);
+console.log(0, 1, 2, 5.6);
+
+console.log(NaN);
+
+// kintamuju uzvadinimas
+const du = 2;
+const antrasSkaicius = 3;
+const PVM = 21;
+
+const a = 5;
+const b = 8; 
+
+console.log('----------------------')
+console.log('primityvios matematines operacijos')
+
+// primityvios matematines operacijos
+
+// suma
+console.log('suna:')
+const suma = a + b;
+console.log(a, b, suma);
+
+
+// skirtumas
+console.log('skirtumas')
+const skirtumas = a - b;
+console.log(a, b, skirtumas);
+
+// sandauga
+console.log('sandauga');
+const sandauga = a * b;
+console.log(a, b, sandauga);
+
+// dalmuo
+console.log('dalmuo');
+const dalmuoAB = a / b;
+const dalmuoBA = b / a;
+console.log(a, b, dalmuoAB, dalmuoBA);
+
+// liekana
+console.log('liekana');
+const liekanaAB = a % b;
+const liekanaBA = b % a;
+console.log(a, b, liekanaAB);
+console.log(a, b, liekanaBA);
+
+// laipsnis
+console.log('laipsnis');
+const laipsnisAB = a ** b; // penki septyntuoju
+const laipsnisBA = b ** a; // septyni penktuoju
+console.log(a, b, laipsnisAB, laipsnisBA);
+
+console.log('-----------------------')
+
+const p1 = 10;
+const p2 = 8;
+const p3 = 3;
+const p4 = 5;
+
+let pazymiuSuma = 0;
+console.log('pazymiu suma: ', pazymiuSuma);
+
+pazymiuSuma = pazymiuSuma + p1;
+console.log('pazymiu suma: ', pazymiuSuma);
+
+pazymiuSuma = pazymiuSuma + p2;
+console.log('pazymiu suma: ', pazymiuSuma);
+
+pazymiuSuma = pazymiuSuma + p3;
+console.log('pazymiu suma: ', pazymiuSuma);
+
+pazymiuSuma = pazymiuSuma + p4;
+console.log('pazymiu suma: ', pazymiuSuma);
+
+console.log('---------------');
+
+const islaida1 = 10;
+const islaida2 = 12;
+const islaida3 = 25;
+const islaida4 = 36;
+
+let saskaita = 100;
+saskaita = saskaita - islaida1;
+console.log('isleidau: ', islaida1, 'saskaitoje liko: ', saskaita);
+
+saskaita -= islaida2;
+console.log('isleidau: ', islaida2, 'saskaitoje liko: ', saskaita);
+
+saskaita -= islaida3;
+console.log('isleidau: ', islaida3, 'saskaitoje liko: ', saskaita);
+
+saskaita -=islaida4;
+console.log('isleidau: ', islaida4, 'saskaitoje liko: ', saskaita);
+
+console.log('-------------------------');
+
+let index = 0;
+console.log('index: ', index++);
+console.log('index: ', index++);
+console.log('index: ', index++);
+console.log('index: ', index++);
+console.log('index: ', index++);
+console.log('index: ', index++);
+
+console.log('------------------')
+
+let index2 = 0;
+console.log('index: ', ++index2);
+console.log('index: ', ++index2);
+console.log('index: ', ++index2);
+console.log('index: ', ++index2);
+console.log('index: ', ++index2);
+console.log('index: ', ++index2);
+
+console.log('------------------')
+
+let index3 = 0;
+console.log('index: ', --index3);
+console.log('index: ', --index3);
+console.log('index: ', --index3);
+console.log('index: ', --index3);
+console.log('index: ', --index3);
+console.log('index: ', --index3);
+
+console.log('------------------')
+
+let index4 = 0;
+console.log('index: ', index4--);
+console.log('index: ', index4--);
+console.log('index: ', index4--);
+console.log('index: ', index4--);
+console.log('index: ', index4--);
+console.log('index: ', index4--);
+
+
+
+
+
+
+
+
