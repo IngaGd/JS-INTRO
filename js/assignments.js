1f548c045340ef06bb7449f046d4ab286c1a6b59
Inga <inga.gudaite@gmail.com>
2022-12-13 20:25:54 +0200

    Assignments

---
+++ js/assignments.js
@@ -0,0 +1,87 @@
+console.clear();
+
+// JavaScript mini užduotys
+
+// Kintamųjų inicijavimas
+
+/* 
+    1. Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+    Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+console.log('1------------numbers:')
+
+const even = 2;
+console.log(even);
+
+const negative = -898;
+console.log(negative);
+
+const decimal = 45.789;
+console.log(decimal);
+
+const infinity = Infinity;
+console.log(infinity);
+
+const undefined = NaN;
+console.log(undefined);
+
+/* 
+    2. Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+    Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+console.log('2------------strings:')
+
+const firstName = 'Tom';
+console.log(firstName);
+
+const secondName = "Sojer";
+console.log(secondName);
+
+const hello = 'Hello, Mr. Tom';
+console.log(hello);
+
+const oddNumbers = '1, 3, 5, 7';
+console.log(oddNumbers);
+
+const random = `ntntktldo37599//`;
+console.log(random);
+
+/* 
+    3. Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+    Po kiekvieno jų inicijavimo, išvesti į console
+*/
+console.log('3----------- three arrays with 5 numbers:')
+const numbers1 = [124, 56, 5859, -9, -787.8];
+console.log(numbers1);
+
+const numbers2 = [NaN, Infinity, -Infinity, 0, 1, 2.3];
+console.log(numbers2);
+
+const numbers3 = [0, 0.2, 1.23, 23, 4];
+console.log(numbers3);
+
+/*
+    4. Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+    Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+console.log('4------------three arrays with 5 text elements:')
+const stringText1 = ['Tom', 'Sam', 'Anna', 'Siri', 'Julia'];
+console.log(stringText1);
+
+const stringText2 = ['Basanaviciaus str.', 'Birutes str', 'Laisves avenue', 'Rudninku square', 'Gedimino avenue'];
+console.log(stringText2);
+
+const stringText3 = ['phisics', 'math', 'psychology', 'geography', 'art']
+console.log(stringText3);
+
+console.log('==============================================================');
+
+//  Veiksmai su kintamaisiais
+
+/*
+    Susumuoti visus skaičiaus tipo kintamuosius
+    Rezultatą išvesti į console
+*/
