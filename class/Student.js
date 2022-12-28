c241754eee8cec542e93df0cc8e5f14f75629b3d
Inga <inga.gudaite@gmail.com>
2022-12-28 13:53:17 +0200

    Student marks average

--- class/Student.js
+++ class/Student.js
@@ -6,11 +6,63 @@ class Student { // klase yra receptura, arba instrikciju rinkinys
         this.shoes = 40;
         this.marks = []; // studentas tures pazymius, bet jis is poradziu yra tuscias sarasas
         console.log('>>>>', vardas);
+    }
+    //metodas - itraukti nauja pazymi
+    addMark(mark) {
+        this.marks.push(mark);
     } 
+    
+    marksAverage(){
+        if (this.marks.length === 0) {
+            return 'neiskaita';
+        }
+        /*const correctMarks = this.marks
+        .filter(m => m >= 1)
+        .filter(m => m <= 10)
+        .filter(m => m % 1 === 0);*/
+        const correctMarks = this.marks
+            .filter(m => m >= 1 && m <= 10 && m % 1 === 0); // si logika efektyvesne, nei auksciau pateikta
+        console.log(correctMarks);
+        return correctMarks.reduce((t, m) => t + m, 0) / correctMarks.length;
+    }
+
+    marksAverage2(){
+        let sum = 0;
+        let correct = 0;
+        for (const mark of this.marks) {
+            if (mark >= 1 && mark <= 10 && mark % 1 === 0) {
+                sum += mark;
+                correct++;
+            }
+        }
+
+        if (correct === 0) {
+            return 'neiskaita';
+        }
+
+        return sum / correct;
+    }
 }
 
 // jei index html nori naudoti Student, vadinasi index turi importuoti tai, ka ekspprtuoja Student. Pirmiausia Student turi eksportuoti, 
 // tada index importuoti. Turi buti dvipusiais mainai.
 
 // module.exports = Student; nebenaudoti
-export default Student; // exportuoja Student reseptura
+// export default Student; // exportuoja Student receptura, t.y. logika
+/*
+class Teacher { // klase yra receptura, arba instrikciju rinkinys
+    constructor(vardas, gimimoMetai) { // studento klases pagrindinis metodas, kuri kvieciame, "vardas" cia yra
+        this.name = vardas; // this.name yra tas vardas, kuri matau is paduotu parametru "Petras" ir "Maryte"
+        this.birthYear = gimimoMetai;
+        this.isMarried = false; // reiksme galime kurti ir metode
+    } 
+}
+
+class Math {
+
+}*/
+
+// export {Student, Teacher, Math}; eksportuoja konkrecia klase su konkreciu pavadinimu
+
+export {Student};
+
--- class/index.js
+++ class/index.js
@@ -7,30 +7,59 @@ console.log('Class...');
 
 // const Student = require('./Student'); // importas, pirmiausia isiimportuoti, tada naudoti. Si sintakse sena, geriau nenaudoti.
 
-import Student from './Student.js'; // reikia pletinio .js ir module package.json
+// import Student from './Student.js'; // reikia pletinio .js ir "module" package.jsone
 // import Studentas from './Student.js'; esant poreikiui eksportuoti vienaip pavadintus, o importuoti kitaip
 // yra globalios klases, pvz "Math" su jau sukurtais metodais, pvz Math.floor, jei sukureme klase "Math", tada neveiks, nebent importuodami pervadinsime i Matematika
-import SimpleStudent from './Student2.js';
+// import SimpleStudent from './Student2.js';
+// import { Student, Teacher, Math as Matieka } from './Student.js'; // tokiu importu isvengiam misstype
+// import * as viskas from './Student.js'; importuoja viska is Student.js, 
+// console.log(viskas);
+// console.log(viskas.petras);
+// console.log(viskas.maryte);
+// console.log(viskas.Teacher);
+// console.log(viskas.Matieka);
+
+import { Student } from "./Student.js";
 
 const petras = new Student('Petras', 2000); // objektas yra kazkas veikiantyis pagal tas instrukcijas,ntai jau pagamintas produktas pagal instrukcijas :)
-const maryte = new SimpleStudent('Marte', 1980); // su new kvieciamas klaseje student esantis konstruktorius, parametra "Maryte" padaveme i construktoriu, del to ispausdina
+// const maryte = new SimpleStudent('Marte', 1980); // su new kvieciamas klaseje student esantis konstruktorius, parametra "Maryte" padaveme i construktoriu, del to ispausdina
+const maryte = new Student('Marte', 1980);
+const jonas = new Student('Jonas', 1990); 
+// const chuck = new Teacher('Chuck', 1960);
 
 // const petras = new Studentas('Petras', 2000); 
 // const maryte = new Studentas('Marte', 1980); 
 
+petras.addMark(10);
+petras.addMark(3.14);
+petras.addMark(0);
+petras.addMark(2);
+petras.addMark(8);
+petras.addMark(4);
+petras.addMark(6);
 
-console.log(petras, petras.name, petras.birthYear, petras.isMarried);
-console.log(maryte, maryte.name, maryte.birthYear, maryte.isMarried);
+maryte.addMark(9);
+maryte.addMark(6);
+
+console.log(petras.name, petras.marks);
+console.log(maryte.name, maryte.marks);
+
+console.log(petras.name, petras.marksAverage());
+console.log(maryte.name, maryte.marksAverage());
+console.log(jonas.name, jonas.marksAverage());
 
 
-console.log(petras);
-console.log(maryte);
-console.log(Student);
+// console.log(petras);
+// console.log(maryte);
+// console.log(Student);
+// console.log(Teacher);
+// console.log(Matieka);
 
+/*
 const suo = {
     vardas: 'Rexas'
 }
-console.log(suo.vardas);
+console.log(suo.vardas);*/
 
 
 
